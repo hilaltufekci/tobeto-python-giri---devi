3aa5b023030f4ca91cbc4eb63e25e741fbc76faf
hilal <hlal.cil@gmail.com>
2023-10-31 11:47:00 +0300

    first commit

---
+++ intro/listeler.py
@@ -0,0 +1,11 @@
+krediler = [
+    "Hızlı Kredi", "Maaşını Halkbanktan alanlara özel",
+    "Mutlu emekli ihtiyaç kredisi"
+]
+print(krediler)
+print(krediler[2])
+print(krediler[0])
+print(len(krediler))
+
+krediler[0] = "Çabuk Kredi"
+print(krediler)
